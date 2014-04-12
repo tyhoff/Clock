c2789edf2dfa2ce00fff26f5badc7ffff25935ef
Tyler H <tyhoffman1@gmail.com>
2014-04-12 13:25:05 -0500

    merge accept with master

--- src/common.h
+++ src/common.h
@@ -1,10 +1,11 @@
 #include <pebble.h>
-#include "helpers.h"
 
 enum {
   REQUEST_ID        = 0x0,
   QUESTION_NUMBER   = 0x1,
 };
+
+char *itoa(int num);
 
 void fill_request_init( uint8_t question_requested );
 void accept_request_init( uint8_t question_requested );
