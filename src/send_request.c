c4ebb8e5ec23fb9b12a9bbe0b9466fecc148497a
Tyler H <tyhoffman1@gmail.com>
2014-04-13 01:27:53 -0500

    add notification layer, add plus/minus

--- src/send_request.c
+++ src/send_request.c
@@ -29,16 +29,25 @@ static Window *window;
 static Layer *main_layer;
 static TextLayer *ticker_layer;
 static TextLayer *text_layer;
+static TextLayer *notification_layer;
 static AppTimer *timer;
 static Bar bars[NUM_BARS];
 static uint8_t question_ticker = 1;
 static BarFill bfill;
+
+static char question_num_text[10];
 
 static BitmapLayer* check_icon_layer;
 static GBitmap *check_icon;
 
 static BitmapLayer* x_icon_layer;
 static GBitmap *x_icon;
+
+static BitmapLayer* plus_icon_layer;
+static GBitmap *plus_icon;
+
+static BitmapLayer* minus_icon_layer;
+static GBitmap *minus_icon;
 
 
 // globals
@@ -169,14 +178,17 @@ static void timer_callback(void *data) {
   }
 
   if (bfill.x >= 50) {
-    text_layer_set_text(text_layer, "Request\nSent");
+    text_layer_set_text(notification_layer, "Request\nSent");
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(notification_layer)); 
     app_timer_register(2000, popView, NULL);
   } else if (bfill.x <= -50) {
-    text_layer_set_text(text_layer, "Request\nVoided");
+    text_layer_set_text(notification_layer, "Request\nCancelled");
+    layer_add_child(window_get_root_layer(window), text_layer_get_layer(notification_layer)); 
     app_timer_register(2000, popView, NULL);
   } else {
     layer_mark_dirty(main_layer);
-    text_layer_set_text(ticker_layer, itoa(question_ticker));
+    snprintf( question_num_text, 10, "Q: %d", question_ticker);
+    text_layer_set_text(ticker_layer, question_num_text);
     timer = app_timer_register(ACCEL_STEP_MS, timer_callback, NULL);  
   }
 
@@ -231,12 +243,12 @@ static void main_layer_update_callback(Layer *me, GContext *ctx) {
 static void draw_letters(Layer *window_layer, GRect bounds) {
   text_layer = text_layer_create((GRect) { .origin = { bounds.size.w/2 - 10 - 50, bounds.size.h/2 - 30}, .size = { 50, 20 } });
   text_layer_set_text(text_layer, "Cancel");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentLeft);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
   text_layer = text_layer_create((GRect) { .origin = { bounds.size.w/2 + 10 , bounds.size.h/2 - 30}, .size = { 50, 20 } });
   text_layer_set_text(text_layer, "Accept");
-  text_layer_set_text_alignment(text_layer, GTextAlignmentCenter);
+  text_layer_set_text_alignment(text_layer, GTextAlignmentRight );
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
   text_layer = text_layer_create((GRect) { .origin = { bounds.size.w/2 +17, 0}, .size = { 50, 50 } });
@@ -244,9 +256,10 @@ static void draw_letters(Layer *window_layer, GRect bounds) {
   text_layer_set_text_alignment(text_layer, GTextAlignmentRight);
   layer_add_child(window_layer, text_layer_get_layer(text_layer));
 
-  ticker_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { 20, 20 } });
-  text_layer_set_text(ticker_layer, itoa(question_ticker));
-  text_layer_set_text_alignment(ticker_layer, GTextAlignmentCenter);
+  ticker_layer = text_layer_create((GRect) { .origin = { 5, 0 }, .size = { 40, 20 } });
+  snprintf( question_num_text, 10, "Q: %d", question_ticker);
+  text_layer_set_text(ticker_layer, question_num_text);
+  text_layer_set_text_alignment(ticker_layer, GTextAlignmentLeft);
   layer_add_child(window_layer, text_layer_get_layer(ticker_layer));
 }
 
@@ -280,6 +293,24 @@ static void window_load( Window * window ) {
   bitmap_layer_set_bitmap(x_icon_layer, x_icon);
   layer_add_child(window_layer, bitmap_layer_get_layer(x_icon_layer));
 
+  plus_icon_layer = bitmap_layer_create(GRect(halfWidth - 6,halfHeight - 55,12,12));
+  plus_icon = gbitmap_create_with_resource(RESOURCE_ID_PLUS_ICON);
+  bitmap_layer_set_bitmap(plus_icon_layer, plus_icon);
+  layer_add_child(window_layer, bitmap_layer_get_layer(plus_icon_layer));
+
+  minus_icon_layer = bitmap_layer_create(GRect(halfWidth - 6,halfHeight + 43,12,12));
+  minus_icon = gbitmap_create_with_resource(RESOURCE_ID_MINUS_ICON);
+  bitmap_layer_set_bitmap(minus_icon_layer, minus_icon);
+  layer_add_child(window_layer, bitmap_layer_get_layer(minus_icon_layer));
+
+  notification_layer = text_layer_create((GRect) { .origin = { 10, 20 }, .size = { 124, 60 } });
+  snprintf( question_num_text, 10, "Q: %d", question_ticker);
+  text_layer_set_text(notification_layer, question_num_text);
+  text_layer_set_background_color( notification_layer, GColorBlack );
+  text_layer_set_text_alignment(notification_layer, GTextAlignmentCenter);
+  text_layer_set_font(notification_layer, fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD));
+  text_layer_set_text_color( notification_layer, GColorWhite );
+
   question_ticker = 1;
 
   accel_data_service_subscribe(0, NULL);
@@ -291,9 +322,13 @@ static void window_unload( Window * window ) {
   question_ticker = 1;
   accel_data_service_unsubscribe();
 
+  gbitmap_destroy(check_icon);
   gbitmap_destroy(x_icon);
+  gbitmap_destroy(plus_icon);
+  gbitmap_destroy(minus_icon);
   text_layer_destroy( text_layer );
   text_layer_destroy( ticker_layer );
+  text_layer_destroy( notification_layer );
   layer_destroy(main_layer);
 }
 
