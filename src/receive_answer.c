0b2223caf5882b90f7b2735551cce900a8f9b284
Tyler H <tyhoffman1@gmail.com>
2014-04-13 10:46:20 -0500

    new receieved

--- src/receive_answer.c
+++ src/receive_answer.c
@@ -3,8 +3,11 @@
 
 static Window *window;
 static TextLayer *text_layer;
+static TextLayer *text_answer;
+static TextLayer *text_question;
 static AppTimer *timer;
 static char answer_text[30];
+static char question_text[30];
 
 // globals
 extern int32_t question_number;
@@ -19,17 +22,34 @@ static void window_load( Window * window ) {
   window_set_background_color( window, GColorBlack );
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 40 } });
-
-  snprintf( answer_text, 30, "Answer to %ld: %c", question_number, (char) answer );
-
-  text_layer_set_text( text_layer, answer_text );
+  text_layer = text_layer_create((GRect) { .origin = { 0, 5 }, .size = { bounds.size.w, 40 } });
+  text_layer_set_text( text_layer, "Answer" );
   text_layer_set_background_color( text_layer, GColorBlack );
-  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
   text_layer_set_text_alignment( text_layer, GTextAlignmentCenter );
   text_layer_set_text_color( text_layer, GColorWhite );
-
   layer_add_child(window_layer, text_layer_get_layer( text_layer ));
+
+  text_answer = text_layer_create((GRect) { .origin = { 0, 50 }, .size = { bounds.size.w, 50 } });
+  // snprintf( answer_text, 30, "%c", (char) answer );
+  snprintf( answer_text, 30, "B" );
+  text_layer_set_text( text_answer, answer_text );
+  text_layer_set_background_color( text_answer, GColorBlack );
+  text_layer_set_font(text_answer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
+  text_layer_set_text_alignment( text_answer, GTextAlignmentCenter );
+  text_layer_set_text_color( text_answer, GColorWhite );
+  layer_add_child(window_layer, text_layer_get_layer( text_answer ));
+
+  text_question = text_layer_create((GRect) { .origin = { 0, 110 }, .size = { bounds.size.w, 40 } });
+  // snprintf( answer_text, 30, "Q: %ld", question_number );
+  snprintf( question_text, 30, "Q: 37" );
+  text_layer_set_text( text_question, "Q: 37" );
+  text_layer_set_background_color( text_question, GColorBlack );
+  text_layer_set_font(text_question, fonts_get_system_font(FONT_KEY_GOTHIC_28));
+  text_layer_set_text_alignment( text_question, GTextAlignmentCenter );
+  text_layer_set_text_color( text_question, GColorWhite );
+  layer_add_child(window_layer, text_layer_get_layer( text_question ));
+
 
   timer = app_timer_register(3000, popView, NULL);
   
