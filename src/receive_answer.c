f647aba19218870b24270e292c4c750e78875d84
Scott Opell <me@scottopell.com>
2014-04-13 04:17:51 -0500

    prettied up the receive answer screen

--- src/receive_answer.c
+++ src/receive_answer.c
@@ -19,13 +19,13 @@ static void window_load( Window * window ) {
   window_set_background_color( window, GColorBlack );
   GRect bounds = layer_get_bounds(window_layer);
 
-  text_layer = text_layer_create((GRect) { .origin = { 0, 0 }, .size = { bounds.size.w, bounds.size.h } });
+  text_layer = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 40 } });
 
   snprintf( answer_text, 30, "Answer to %ld: %c", question_number, (char) answer );
 
   text_layer_set_text( text_layer, answer_text );
   text_layer_set_background_color( text_layer, GColorBlack );
-  text_layer_set_font(text_layer, fonts_get_system_font( FONT_KEY_GOTHIC_14 ));
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD));
   text_layer_set_text_alignment( text_layer, GTextAlignmentCenter );
   text_layer_set_text_color( text_layer, GColorWhite );
 
