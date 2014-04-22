0ff7d39de5553d41b15eb09c023906c455ba5308
Scott Opell <me@scottopell.com>
2014-04-22 10:45:55 -0400

    removed debugging code from server js

--- config-page/public/clock-config.js
+++ config-page/public/clock-config.js
@@ -5,31 +5,6 @@ var getParameterByName = function(name) {
   return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 };
 
-function simpleObjInspect(oObj, key, tabLvl) {
-    key = key || "";
-    tabLvl = tabLvl || 1;
-    var tabs = "";
-    for(var i = 1; i < tabLvl; i++){
-        tabs += "\t";
-    }
-    var keyTypeStr = " (" + typeof key + ")";
-    if (tabLvl == 1) {
-        keyTypeStr = "(self)";
-    }
-    var s = tabs + key + keyTypeStr + " : ";
-    if (typeof oObj == "object" && oObj !== null) {
-        s += typeof oObj + "\n";
-        for (var k in oObj) {
-            if (oObj.hasOwnProperty(k)) {
-                s += simpleObjInspect(oObj[k], k, tabLvl + 1);
-            }
-        }
-    } else {
-        s += "" + oObj + " (" + typeof oObj + ") \n";
-    }
-    return s;
-}
-
 $(function() {
   var item = window.localStorage.getItem('config');
   var roomId;
@@ -39,7 +14,6 @@ $(function() {
     config = {};
   } else {
     config = JSON.parse(item);
-    console.log("CCCCCCCCONFIG" + simpleObjInspect(item));
 
     if ( config.hasOwnProperty("room-id") ){
       roomId = config['room-id'];
